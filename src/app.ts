c0cd758965e725823d8e8e8986cfb2c764eb9085
NikKowPHP <nik.kow@outlook.com>
2024-12-15 12:07:31 +0100

    fix init start

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import './alias';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { config } from './config';
-import routes from './routes';
-import { errorHandler } from './middleware/errorHandler';
+import { config } from '@/config';
+import routes from '@/routes';
+import { errorHandler } from '@/middleware/errorHandler';
 
 const app = express();
 
